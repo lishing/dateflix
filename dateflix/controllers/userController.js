dea362c7febd013d784d09a342bed6f4e31bcc93
Li Shing Chan <lischan@microsoft.com>
2020-07-31 22:58:19 +0800

    controllers dependencies

---
+++ dateflix/controllers/userController.js
@@ -0,0 +1,4 @@
+const logRepository = require('../repositories/logRepository');
+const db = require('../db');
+const { update, getMany } = require('../repositories/logRepository');
+const { data } = require('jquery');
