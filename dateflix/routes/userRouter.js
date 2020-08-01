760f799c4385298535a6bfbc343621b5eee2c46a
stephanieahx <stephanieanghs@gmail.com>
2020-08-01 20:25:52 +0800

    started post route for user registration

--- dateflix/routes/userRouter.js
+++ dateflix/routes/userRouter.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 
 router.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    //validation
-    if (!username || !password) {
-        return res.status(400).json({ msg: "Not all fields have been entered" });
+    try {
+        const { username, password } = req.body;
+        //validation
+        if (!username || !password) {
+            return res
+                .status(400)
+                .json({ msg: "Not all fields have been entered" });
+        }
+        if (password.length < 5) {
+            return res
+                .status(400)
+                .json({ msg: "Password has to be at least five characters" });
+        }
+    } catch (err) {
+        res.status(500).json({ err });
     }
-    if (password.length < 5) {
-        return res.status(400).json({ msg: "Password has to be at least five characters" });
-    }
-})
+});
 
 module.exports = router;
