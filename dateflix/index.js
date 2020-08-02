97d57b8f0a174f846e7b113663a81dc11bc19c03
Li Shing Chan <lischan@microsoft.com>
2020-08-02 10:31:10 +0800

    method-override

--- dateflix/index.js
+++ dateflix/index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const methodOverride = require('method-override')
 
 const app = express();
 
 // Middleware
 app.use(express.json()); // json body parser 
 app.use(cors());
-
+app.use(methodOverride('_method'))
 const PORT = process.env.PORT || 3000;
 
+//test if it's deployed on heroku/port
 app.get("/status", (req,res) => res.status(200).json({status: "deployed on heroku"}) )
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
 
 
 // Set up Mongoose
