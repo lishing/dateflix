138a1bc6373264f542c1c48c77c2b65c8c826457
Li Shing Chan <lischan@microsoft.com>
2020-08-04 00:02:01 +0800

    user authentification v1 done

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
     try {
@@ -14,15 +16,25 @@ router.post("/register", async (req, res) => {
                 .status(400)
                 .json({ msg: "Password has to be at least eight characters" });
         }
-        const existingUser = await User.find({username: username})
+        const existingUser = await User.findOne({username: username})
         if (existingUser) {
             return res
             .status(400)
             .json({msg: "An account with this email already exists."}); 
         }
+        if (!username) username = email;
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
         
+        const newUser = new User({
+            email,
+            password: passwordHash,
+            username
+        });
+        const savedUser = await newUser.save();
+        res.json(savedUser);
     } catch (err) {
-        res.status(500).json({ err });
+        res.status(500).json({err});
     }
 });
 
