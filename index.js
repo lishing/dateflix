ed4214ee15878002f3a740b7cb70d4a397de9ae4
Li Shing Chan <lischan@microsoft.com>
2020-08-03 23:28:03 +0800

    folder structure

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const app = express();
 
 // Middleware
 app.use(express.json()); // json body parser 
-// app.use(cors());
+//app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.get("/status", (req,res) => res.status(200).json({status: "deployed on heroku"}));
 
 // Set up Mongoose
-mongoose.connect(
-    process.env.MONGODB_CONNECTION_STRING,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    (err) => {
-        if (err) throw err;
-        console.log("MongoDB connection established")
-    }
-);
+// mongoose.connect(
+//     process.env.MONGODB_CONNECTION_STRING,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     },
+//     (err) => {
+//         if (err) throw err;
+//         console.log("MongoDB connection established")
+//     }
+// );
 
 // Set up routes
-app.use("/users", require("./src/routes/userRouter"));
+app.get("/users", require("./src/routes/userRouter"));
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
