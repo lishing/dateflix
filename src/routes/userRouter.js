e15cfedf138d1c213ce7400d87bff5fb3278a478
Li Shing Chan <lischan@microsoft.com>
2020-08-03 23:47:03 +0800

    user registration verification

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -9,11 +9,18 @@ router.post("/register", async (req, res) => {
                 .status(400)
                 .json({ msg: "Not all fields have been entered" });
         }
-        if (password.length < 5) {
+        if (password.length < 8) {
             return res
                 .status(400)
-                .json({ msg: "Password has to be at least five characters" });
+                .json({ msg: "Password has to be at least eight characters" });
         }
+        const existingUser = await User.find({username: username})
+        if (existingUser) {
+            return res
+            .status(400)
+            .json({msg: "An account with this email already exists."}); 
+        }
+        
     } catch (err) {
         res.status(500).json({ err });
     }
